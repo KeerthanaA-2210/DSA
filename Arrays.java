7bfc3fa2419cb3c9e306afb31d559bb35c7a1fc9
Keerthana A <keerthana22102005@gmail.com>
2026-07-27 15:13:30 +0530

    Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -264,3 +264,29 @@ public class Main {
         System.out.println("The missing number is "+num);
     }
 }
+
+// Pattern: traversal+count-Bruteforce
+//Find the majority element (appears more than n/2 times)
+// Time Complexity: O(n2)
+// Space Complexity: O(1)
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[]={2,5,5,5,2,5};
+        int n=arr.length/2;
+        
+
+        for (int i = 0; i < arr.length; i++) {
+            int count=0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]==arr[j])
+                    count++;          
+            }
+            if(count>n) {  
+            System.out.println(arr[i]+" is the major number");
+        break;
+    }
+        }
+        
+    }
+}
