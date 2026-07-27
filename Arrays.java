9b6687a4913df4c688baf4ad493c90b18b725348
Keerthana A <keerthana22102005@gmail.com>
2026-07-27 12:34:40 +0530

    Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -214,3 +214,29 @@ public class Main {
         
     }
 }
+//rotate array-Pattern:Reversal Pattern
+//T(n)=O(n)
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int start=0;
+        int end=arr.length-1;
+        int k=2;k=k%arr.length;
+        reverse(arr, start, end);
+        reverse(arr, 0, k-1);
+        reverse(arr, k,arr.length-1);
+        for (int i = 0; i < arr.length;i++)     
+        System.out.println(arr[i]);
+      
+    }
+    static void reverse(int arr[],int start,int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
