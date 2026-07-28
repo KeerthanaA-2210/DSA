352c6e8c7d8a2c0ec99df6b69b5f4fed45adb062
Keerthana A <keerthana22102005@gmail.com>
2026-07-28 07:57:02 +0530

    Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -419,3 +419,29 @@ public class Main {
 
     }
 }
+
+// Pattern:Traversal+counting
+// Find the cout of maximum consecutive one(1's)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+public class Main {
+    public static void main(String[] args) {
+    int[] arr={0, 1, 1,1,1 ,1,0,1,1,1,1};
+    int count = 0;
+    int maxCount = 0;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]==1){
+            count++;
+        }
+        else{
+            count=0;
+        }
+        if(count>maxCount){
+        maxCount=count;
+    }
+    }
+    
+    System.out.println(maxCount);
+
+    }
+}
