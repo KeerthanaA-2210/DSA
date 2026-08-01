ef4bf6b4f97985e1deaeb7a86302f97fac843bd7
Keerthana A <keerthana22102005@gmail.com>
2026-08-01 22:51:07 +0530

    Strings.java

--- Strings.java
+++ Strings.java
@@ -30,3 +30,108 @@ public class small {
          }
     }  
 }
+//Count frequency of characters
+import java.util.*;
+public class Main{
+     public static void main(String[] args) {
+      
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter the string: ");
+        String inpuString=sc.next();
+        StringBuilder s=new StringBuilder(inpuString);
+        
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i), map.get(s.charAt(i))+1);
+            }
+            else{
+                map.put(s.charAt(i),1);
+            }
+        }
+        System.out.print(map);
+}
+}
+
+//Remove spaces hi world -> hiworld
+import java.util.*;
+public class Main{
+     public static void main(String[] args) {
+      
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter the string: ");
+        String inpuString=sc.nextLine();
+        StringBuilder s=new StringBuilder(inpuString);
+
+         StringBuilder h=new StringBuilder();
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!=' '){
+                h.append(s.charAt(i));
+            }
+            
+        }
+        for(int i=0;i<h.length();i++)
+        System.out.print(h.charAt(i));
+}
+}
+//Toggle Case 
+//Input - HeLLo  O/p= hEllO
+import java.util.*;
+public class Main{
+     public static void main(String[] args) {
+      
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter the string: ");
+        String inpuString=sc.nextLine();
+        StringBuilder s=new StringBuilder(inpuString);
+
+         StringBuilder h=new StringBuilder();
+        
+        for(int i=0;i<s.length();i++){
+            char chh=s.charAt(i);
+            if(Character.isUpperCase(chh)){
+                h.append(Character.toLowerCase(chh));
+            }
+            else{
+                h.append(Character.toUpperCase(chh));
+            }
+            
+        }
+        
+        System.out.print(h);
+}
+}
+//Count words 
+import java.util.*;
+public class Main{
+     public static void main(String[] args) {
+      
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter the string: ");
+        String inpuString=sc.nextLine();
+       
+        StringBuilder s=new StringBuilder(inpuString);
+
+        int count=0;
+        if(s.length()==0){ 
+            System.out.println(0);
+            return;
+        }
+        else{
+        
+        for(int i=0;i<s.length();i++){
+            char chh=s.charAt(i);
+            if(chh!=' '&&(i==0||s.charAt(i-1)==' ')){
+               count++;
+            }       
+        }
+    }
+        System.out.print(count);
+}
+}
