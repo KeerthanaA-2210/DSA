d24e2a2e9feb099d0e7f5192e2b19b69ed33d477
Keerthana A <keerthana22102005@gmail.com>
2026-08-05 15:50:37 +0530

    Strings.java

--- Strings.java
+++ Strings.java
@@ -135,3 +135,20 @@ public class Main{
         System.out.print(count);
 }
 }
+import java.util.*;
+//Reverse Each Word  Hi all --> iH lla
+public class Main{
+     public static void main(String[] args) {
+      
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        
+        String words[]=s.split(" ");
+        
+        for(int i=0;i<words.length;i++){
+            for(int j=words[i].length()-1;j>=0;j--)
+            System.out.print(words[i].charAt(j));
+        System.out.print(" ");
+        }
+}
+}
