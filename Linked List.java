2d68df36d4ef1581dcd729ac0386ca0afc8c8b2f
Keerthana A <keerthana22102005@gmail.com>
2026-08-07 12:36:12 +0530

    Create Linked List.java

---
+++ Linked List.java
@@ -0,0 +1,57 @@
+//Linked List Traversal + Length of LL + Print LL + Searching
+import java.util.*;
+ class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+ }
+public class linkedlist{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of nodes: ");
+        int n=sc.nextInt();
+        System.out.println("Enter target ");
+        int target=sc.nextInt();
+        Node head=null;
+        Node tail=null;
+        boolean found=false;
+
+        System.out.println("Enter values : ");
+        for(int i=0;i<n;i++){
+            int value=sc.nextInt();
+            Node newnode =new Node(value);
+
+            if(head==null){
+                head=newnode;
+                tail=newnode;
+            }
+            else{
+                tail.next=newnode;
+                tail=newnode;
+            }
+        }
+        Node temp=head;int count=0;
+        while(temp!=null){
+            count++;
+            System.out.println(temp.data+" " );
+            if(temp.data==target){
+                found=true;
+            }   
+            temp=temp.next;
+            
+        }
+        System.out.println("Length of LL "+count);
+        if(found==true){
+            System.out.println("target found");
+        }
+        else{
+            System.out.println("not found");
+        }
+       
+    }
+    }
+
