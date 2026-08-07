c7901c05448b138b74e57c64e3f143b8d3f83099
Keerthana A <keerthana22102005@gmail.com>
2026-08-07 15:27:15 +0530

    Update Linked List.java

--- Linked List.java
+++ Linked List.java
@@ -111,4 +111,53 @@ public class linkedlist{
     }
     
 }
+//Linked List - Middle of LL
+import java.util.*;
+ class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+ }
+public class linkedlist{
+    public static void main(String args[]){
+       Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of nodes: ");
+        int n=sc.nextInt();
+       
+        Node head=null;
+        Node tail=null;
+        System.out.println("Enter values : ");
+        for(int i=0;i<n;i++){
+            int value=sc.nextInt();
+            Node newnode =new Node(value);
+
+            if(head==null){
+                head=newnode;
+                tail=newnode;
+            }
+            else{
+                tail.next=newnode;
+                tail=newnode;
+            }
+        }
+        Node res=MiddleList(head);
+        System.out.println("The middle Node is "+res.data);
+
+    }
+    public static Node MiddleList(Node head){
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    
+}
+
 
