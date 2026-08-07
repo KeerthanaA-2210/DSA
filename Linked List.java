2f72587c02c8a8f5c4f41ebb86faa43efc824198
Keerthana A <keerthana22102005@gmail.com>
2026-08-07 13:47:22 +0530

    Update Linked List.java

--- Linked List.java
+++ Linked List.java
@@ -55,3 +55,66 @@ public class linkedlist{
     }
     }
 
+//Linked List - Reverse LL
+import java.util.*;
+ class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+ }
+public class linkedlist{
+    public static void main(String args[]){
+       Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of nodes: ");
+        int n=sc.nextInt();
+       
+        Node head=null;
+        Node tail=null;
+        boolean found=false;
+
+        System.out.println("Enter values : ");
+        for(int i=0;i<n;i++){
+            int value=sc.nextInt();
+            Node newnode =new Node(value);
+
+            if(head==null){
+                head=newnode;
+                tail=newnode;
+            }
+            else{
+                tail.next=newnode;
+                tail=newnode;
+            }
+        }
+
+        Node temp=head;
+        while (temp!=null) {
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }System.out.println("");
+        //Reverse the LL
+    Node prev = null;
+    Node curr = head;
+    Node next = null;
+
+    while (curr != null) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+}
+
+head = prev;
+temp=head;
+while (temp!=null) {
+            System.out.print(" "+temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    
+}
+
